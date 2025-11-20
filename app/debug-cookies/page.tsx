120e182825fe831ee4ed603c0d6721f671e08cb5
ReliefHommy <reliefthai@gmail.com>
2025-11-21 00:12:39 +0100

    fix debug-cookies page

--- app/debug-cookies/page.tsx
+++ app/debug-cookies/page.tsx
@@ -1,6 +1,16 @@
 // app/debug-cookies/page.tsx
 import { cookies } from 'next/headers'
-export default function DebugCookies() {
-  const all = cookies().getAll().map(c => `${c.name}=${c.value.slice(0,12)}…`)
-  return <pre className="p-4">{JSON.stringify(all, null, 2)}</pre>
+
+export default async function DebugCookies() {
+  const cookieStore = await cookies()
+  const all = cookieStore.getAll().map(
+    c => `${c.name}=${c.value.slice(0, 12)}…`
+  )
+
+  return (
+    <pre className="p-4">
+      {JSON.stringify(all, null, 2)}
+    </pre>
+  )
 }
+
