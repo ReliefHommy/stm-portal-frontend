1d8c4d8c83fe6370818c25e5e7ad24a5f82760a9
ReliefHommy <reliefthai@gmail.com>
2025-12-15 16:10:28 +0700

    Add hostname shown in image_url

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-2a72cfdc56dc408cbaa236e02a09710b.r2.dev',
+        
+      },
+    ],
+  },
 
   // other config...
 
