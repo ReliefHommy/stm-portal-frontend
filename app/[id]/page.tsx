10902989683e78eb3e293c3005f9bcb1242074e3
ReliefHommy <reliefthai@gmail.com>
2025-12-15 18:52:03 +0700

    Add STM-Post Detail page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 
 async function getPost(id: string) {
-  const res = await fetch(`https://somtammarket.com/studio/posts/${id}/`)
+  const res = await fetch(`https://stm-food-backend-production.up.railway.app/api/studio/stm-post/${id}/`)
   return res.json()
 }
 
@@ -11,7 +11,7 @@ export default async function PostDetail({ params }: any) {
   return (
     <div className="px-4 py-10 max-w-3xl mx-auto">
       <Image
-        src={post.image}
+        src={post.image_url || ''}
         alt={post.title}
         width={1200}
         height={900}
